62c68edee22ef940537169bc7bf55123b309638e
Suhud Cryptohits <suhudsuhudi@gmail.com>
2026-02-06 08:51:59 +0000

    Try fixing this error: `Runtime SecurityError: Failed to read a named pr

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type {NextConfig} from 'next';
 
 const securityHeaders = [
   {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-  },
-  {
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
